33d4096f28507f4bafe4273155b71ab446d8ad11
ryankynor <ryankynor@hanovernorwichschools.org>
2016-01-05 14:48:38 -0500

    Updated from Brython Server: 1/5/2016 2:48:38 PM

--- Finalproject.py
+++ Finalproject.py
@@ -1,1 +1,17 @@
+"""
+Ryan Kynor
+Geostationary orbit calculator
+"""
 
+staticmass = input("What is the static mass of the rocket?")
+fuelmass = input("What is the mass of the fuel in the rocket?")
+thrustvelocity = input("What is the thrust velocity of the engine used in the rocket?")
+
+
+
+
+
+
+
+if delta-v >= 13500:
+    print("Yay! Your rocket made it to geostatonary orbit!")
