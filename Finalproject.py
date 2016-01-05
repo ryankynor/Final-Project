703aa6419562535c1c5be1e3c4e423037cbd489d
ryankynor <ryankynor@hanovernorwichschools.org>
2016-01-05 14:49:09 -0500

    Updated from Brython Server: 1/5/2016 2:49:10 PM

--- Finalproject.py
+++ Finalproject.py
@@ -2,7 +2,10 @@
 Ryan Kynor
 Geostationary orbit calculator
 """
+import math
 
+
+delta-v = 0
 staticmass = input("What is the static mass of the rocket?")
 fuelmass = input("What is the mass of the fuel in the rocket?")
 thrustvelocity = input("What is the thrust velocity of the engine used in the rocket?")
