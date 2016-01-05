97ec45ac9b87e232f0655c8ed73d740cbafd6d11
ryankynor <ryankynor@hanovernorwichschools.org>
2016-01-05 14:53:25 -0500

    Updated from Brython Server: 1/5/2016 2:53:26 PM

--- Finalproject.py
+++ Finalproject.py
@@ -11,10 +11,12 @@ fuelmass = input("What is the mass of the fuel in the rocket?")
 thrustvelocity = input("What is the thrust velocity of the engine used in the rocket?")
 
 
-delta-v = 
+#delta-v = 
 
 
 
 
 if delta-v >= 13500:
     print("Yay! Your rocket made it to geostatonary orbit!")
+else:
+    print("Your rocket didn't make it to geostationary orbit.")
